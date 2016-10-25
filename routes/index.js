7ec185aabd86c1c945c18a4f5442b3d641f9c3d6
Anupam Bhaglania <anupamb@brdsys.com>
2016-10-25 18:21:01 +0530

    single user pipeline

--- routes/index.js
+++ routes/index.js
@@ -49,6 +49,31 @@ router.post('/timely', function(req, res, next) {
 
 });
 
+router.post('/user', function(req, res, next) {
+  var reqData = req.body;
+  console.log(reqData);
+
+  var reqOptions = {
+      url: 'http://104.155.210.101:8081/user',
+      body: reqData,
+      timeout: 120000,
+      json:true,
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Connection" : "keep-alive"
+      }
+  };
+
+    try{
+        request.post(reqOptions).pipe(res);
+    }catch(e){
+        console.log(e);
+    }
+
+});
+
+
 router.get('/map', function(req, res, next) {
   var reqOptions = {
       url: 'http://104.155.210.101:8081/images/map.png',
