9d3509a2bc102f7507ec52cfdf1ebe8a1ea705d3
Anupam Bhaglania <anupamb@brdsys.com>
2016-10-25 11:26:33 +0530

    hello_routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var router = express.Router();
 
+var request = require('request');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/daily', function(req, res, next) {
+  var reqData = req.body;
+
+  var reqOptions = {
+      url: 'http://104.155.210.101:8081/daily',
+      body: reqData,
+      json: true,
+      timeout: 120000,
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Connection" : "keep-alive"
+      }
+  };
+
+  request.post(reqOptions).pipe(res);
+
 });
+
+router.get('/timely', function(req, res, next) {
+  var reqData = req.body;
+
+  var reqOptions = {
+      url: 'http://104.155.210.101:8081/timely',
+      body: reqData,
+      json: true,
+      timeout: 120000,
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Connection" : "keep-alive"
+      }
+  };
+
+  request.post(reqOptions).pipe(res);
+
+});
+
+router.get('/', function(req, res, next) {
+  request.post(req).pipe(res);
+
+});
+
 
 module.exports = router;
